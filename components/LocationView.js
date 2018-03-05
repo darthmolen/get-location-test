a9bdd89680034d821939b9d42d15477c27876856
Steven_Molen <darthmolen@gmail.com>
2018-03-04 20:58:29 -0600

    initial commit

---
+++ components/LocationView.js
@@ -0,0 +1,7 @@
+import { Text, View, StyleSheet } from 'react-native'
+
+const LocationView = ({text}) => (
+    <View style={style.container}>
+        <Text>{text}</Text>
+    </View>
+)
---
+++ components/LocationViewContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+//import LocationView from '../components/LocationView';
+import { Text } from 'react-native';
+
+export default class LocationViewContainer extends Component {
+  state = {
+    location: null,
+    errorMessage: null
+  };      
+
+  componentWillMount() {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        this.setState({
+          errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        });
+      } else {
+        this._getLocationAsync();
+      }
+    }
+  
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  render() {
+    let text = "Waiting....";
+    if (this.state.errorMessage){
+      text = errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
+    return (<Text>This is a test</Text>);
+  }
+
+}
